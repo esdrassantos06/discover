2d2f68e99d0561f51295a606b4af15d9de3b2fd0
esdrassantos06 <esdrasirion1@gmail.com>
2025-02-22 22:32:47 +0000

    changing icons just for meme

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -5,16 +5,25 @@ const SocialMediaIcons = () => {
     return (
         <div className='flex space-x-4'>
             <div className='hover:bg-surfaceLightModeHover dark:hover:bg-surfaceDarkModeHover transition-all duration-700 ease-in-out p-2 rounded-full'>
-                <Github size={26} />
+
+                <a href="https://github.com" target='_blank'>
+                    <Github size={26} />
+                </a>
             </div>
             <div className='hover:bg-surfaceLightModeHover dark:hover:bg-surfaceDarkModeHover transition-all duration-700 ease-in-out p-2 rounded-full'>
-                <Instagram size={26} />
+                <a href="https://instagram.com" target='_blank'>
+                    <Instagram size={26} />
+                </a>
             </div>
             <div className='hover:bg-surfaceLightModeHover dark:hover:bg-surfaceDarkModeHover transition-all duration-700 ease-in-out p-2 rounded-full'>
-            <Youtube size={26} />
+                <a href="https://youtube.com" target='_blank'>
+                    <Youtube size={26} />
+                </a>
             </div>
             <div className='hover:bg-surfaceLightModeHover dark:hover:bg-surfaceDarkModeHover transition-all duration-700 ease-in-out p-2 rounded-full'>
-            <Linkedin size={26} />
+                <a href="https://linkedin.com" target='_blank'>
+                    <Linkedin size={26} />
+                </a>
             </div>
         </div>
 
