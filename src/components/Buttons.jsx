d3f707a6356a4701791bb88a7bde3433958b8c7e
esdrassantos06 <esdrasirion1@gmail.com>
2025-02-22 22:36:08 +0000

    update to tailwind v4

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -10,7 +10,7 @@ const arrText = [
 return(
     <div className='flex flex-col dark:text-darkModeText text-lightModeText justify-center gap-4'>
         {arrText.map(item => (
-            <button key={item.id} className='border w-96 h-12 border-solid dark:border-lightModeBackground border-darkModeBackground hover:border-darkModeBackground hover:border-opacity-100 border-opacity-50  dark:border-opacity-50 bg-surfaceLightMode hover:bg-surfaceLightModeHover dark:bg-surfaceDarkMode dark:hover:bg-surfaceDarkModeHover dark:hover:border-lightModeBackground transition-all ease-out duration-500 font-medium text-sm px-4 py-2 rounded-md focus:outline-none'>
+            <button key={item.id} className='border w-96 h-12 border-solid dark:border-lightModeBackground border-darkModeBackground hover:border-darkModeBackground hover:border-opacity-100 border-opacity-50  dark:border-opacity-50 bg-surfaceLightMode hover:bg-surfaceLightModeHover dark:bg-surfaceDarkMode dark:hover:bg-surfaceDarkModeHover dark:hover:border-lightModeBackground transition-all ease-out duration-500 font-medium text-sm px-4 py-2 rounded-md focus:outline-hidden'>
                 {item.name}
             </button>
         ))}
--- src/components/DarkModeButton.jsx
+++ src/components/DarkModeButton.jsx
@@ -24,7 +24,7 @@ const DarkModeSwitch = () => {
         <div>        
         <button id="switch"
             onClick={toggleDarkMode}
-            className="w-12 z-10 h-5 rounded-2xl bg-lightModeBackground border-surfaceLightMode dark:border-surfaceDarkMode border border-opacity-10 border-solid bg-opacity-10 hover:bg-opacity-5 flex items-center transition duration-300 focus:outline-none">
+            className="w-12 z-10 h-5 rounded-2xl bg-lightModeBackground border-surfaceLightMode dark:border-surfaceDarkMode border border-opacity-10 border-solid bg-opacity-10 hover:bg-opacity-5 flex items-center transition duration-300 focus:outline-hidden">
 
             <div className={`relative z-10 ${isDarkMode ? 'translate-x-1' : 'translate-x-11'} duration-500 flex bg-lightModeBackground dark:hover:outline-surfaceDarkModeHover  outline-surfaceLightModeHover hover:outline-8 hover:outline transition-all  items-center justify-center rounded-full h-7 w-7 -left-4`}>
                     {isDarkMode ? <MoonStars size={17} className='text-black' /> : <Sun size={17} className='text-black' />}
